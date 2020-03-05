9da732f6baf7626a322189887725379a4410f9bf
Maciej Bak <wsciekly.maciek@gmail.com>
2020-03-05 17:32:53 +0100

    added pkg instalation bash script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+###############################################################################
+#
+#   Install all the software: tools & packages
+#
+#   AUTHOR: Maciej_Bak
+#   AFFILIATION: Swiss_Institute_of_Bioinformatics
+#   CONTACT: wsciekly.maciek@gmail.com
+#   CREATED: 05-03-2020
+#   LICENSE: GPL_v3.0
+#
+###############################################################################
+
+date
