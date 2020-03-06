9133bf674920cc8f68db68ab6804443d26d7fc9e
Maciej Bak <wsciekly.maciek@gmail.com>
2020-03-06 11:32:50 +0100

    updated installation commands 4.6

--- setup.sh
+++ setup.sh
@@ -13,10 +13,13 @@
 ###############################################################################
 
 
-yes | pkg install git
+yes | pkg install \
+  git=2.25.1
 
+yes | pkg install \
+  curl=7.68.0 \
+  gnupg=2.2.19
 
-yes | pkg install curl gnupg
 mkdir -p "$PREFIX/etc/apt/sources.list.d/"
 echo "deb https://its-pointless.github.io/files/ termux extras" \
 > "$PREFIX/etc/apt/sources.list.d/pointless.list"
@@ -24,27 +27,18 @@ curl "https://its-pointless.github.io/pointless.gpg" | apt-key add
 
 apt-get update
 
-yes | pkg install r-base \
-            make \
-            clang \
-            gcc-7 \
-            libgfortran3 \
-            openssl \
-            libcurl \
-            libicu \
-            libxml2 \
-            python \
+yes | pkg install \
+  r-base=3.6.1-4 \
+  make=4.3-1 \
+  clang=9.0.1-1 \
+  gcc-7=7.4.0-2 \
+  libgfortran3=6.5.0-2 \
+  openssl \
+  libcurl \
+  libicu=65.1 \
+  libxml2=2.9.10 \
+  python=3.8.2 \
 
-# r-base=3.6.1-4
-# make 4.3-1
-# clang 9.0.1-1
-# gcc-7 7.4.0-2
-# libgfortran3 6.5.0-2
-# openssl ???
-# libcurl ???
-# libicu 65.1
-# libxml 2.9.10
-# python 3.8.2
 # scipy 1.3.0-1
 
 #export LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib
@@ -53,14 +47,14 @@ yes | pkg install r-base \
 # setupclang-gfort-7
 
 pip install \
-pandas==1.0.1 \
-scikit-learn==0.22.2 \
+numpy \
 statsmodels==0.11.1 \
+scikit-learn==0.22.1 \
 #Keras==2.3.1
+#pandas==1.0.1 \
 
 #R -e "install.packages('maxLik', repos='http://cran.us.r-project.org')"
 
 # Rscript
-# scipy
 
 apt clean
