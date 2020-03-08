613afe690f53e16ecfe5209d20aa8635a00460c7
Maciej Bak <wsciekly.maciek@gmail.com>
2020-03-09 00:34:40 +0100

    reduced setup

--- setup.sh
+++ setup.sh
@@ -33,13 +33,23 @@ pkg install -y \
   make=4.3-1 \
   clang=9.0.1-1 \
   gcc-9=9.2.0-1 \
-  openssl=1.1.1d-1 \
-  libcurl=7.68.0 \
-  libicu=65.1 \
-  libxml2=2.9.10 \
   python=3.8.2 \
   scipy=1.4.1 \
   r-cran-tidyverse=1.3.0
+
+
+#pkg install -y \
+#  r-base=3.6.2 \
+#  make=4.3-1 \
+#  clang=9.0.1-1 \
+#  gcc-9=9.2.0-1 \
+#  openssl=1.1.1d-1 \
+#  libcurl=7.68.0 \
+#  libicu=65.1 \
+#  libxml2=2.9.10 \
+#  python=3.8.2 \
+#  scipy=1.4.1 \
+#  r-cran-tidyverse=1.3.0
 
 # compiler configuration
 setupgcc-9
@@ -52,6 +62,8 @@ R -e "install.packages('maxLik', repos='http://cran.us.r-project.org')"
 R -e "install.packages('numDeriv', repos='http://cran.us.r-project.org')"
 
 pip install \
+pyyaml==5.3 \
+jinja2==2.11.1 \
 cython==0.29.15 \
 pandas==1.0.1 \
 
